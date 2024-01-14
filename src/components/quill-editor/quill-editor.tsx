00524b6f3ccdd9fbc77fe1afbe0fb07309e6fa70
Akshay <aksingh240@gmail.com>
2024-01-14 18:05:47 +0530

    added breadcrumbs

--- src/components/quill-editor/quill-editor.tsx
+++ src/components/quill-editor/quill-editor.tsx
@@ -83,8 +83,42 @@ const QuillEditor: React.FC<QuillEditorProps> = ({
   }, [state, workspaceId, folderId]);
 
   const breadCrumbs = useMemo(() => {
-    return "";
-  }, [state]);
+    if (!pathname || !state.workspaces || !workspaceId) return;
+    const segments = pathname
+      .split("/")
+      .filter((val) => val !== "dashboard" && val);
+    const workspaceDetails = state.workspaces.find(
+      (workspace) => workspace.id === workspaceId
+    );
+    const workspaceBreadCrumb = workspaceDetails
+      ? `${workspaceDetails.iconId} ${workspaceDetails.title}`
+      : "";
+    if (segments.length === 1) {
+      return workspaceBreadCrumb;
+    }
+
+    const folderSegment = segments[1];
+    const folderDetails = workspaceDetails?.folders.find(
+      (folder) => folder.id === folderSegment
+    );
+    const folderBreadCrumb = folderDetails
+      ? `/ ${folderDetails.iconId} ${folderDetails.title}`
+      : "";
+
+    if (segments.length === 2) {
+      return `${workspaceBreadCrumb} ${folderBreadCrumb}`;
+    }
+
+    const fileSegment = segments[2];
+    const fileDetails = folderDetails?.files.find(
+      (file) => file.id === fileSegment
+    );
+    const fileBreadCrumb = fileDetails
+      ? `/ ${fileDetails.iconId} ${fileDetails.title}`
+      : "";
+
+    return `${workspaceBreadCrumb} ${folderBreadCrumb} ${fileBreadCrumb}`;
+  }, [state, pathname, workspaceId]);
 
   const wrapperRef = useCallback(async (wrapper: any) => {
     if (typeof window !== undefined) {
